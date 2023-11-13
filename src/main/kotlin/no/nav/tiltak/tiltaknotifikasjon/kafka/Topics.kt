8ceb16c3abf3a8abda35ffc24bbfc919b138ce1c
Sindre Dahl Løken <sindre.loken@nav.no>
2023-11-13 12:26:27 +0100

    lagt til topics

---
+++ src/main/kotlin/no/nav/tiltak/tiltaknotifikasjon/kafka/Topics.kt
@@ -0,0 +1,6 @@
+package no.nav.tiltak.tiltaknotifikasjon.kafka
+
+object Topics {
+    const val AVTALE_HENDELSE = "arbeidsgiver.tiltak-avtale-hendelse"
+    const val AVTALE_HENDELSE_COMPACT = "arbeidsgiver.tiltak-avtale-hendelse-compact"
+}
