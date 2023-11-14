e7eebfc52b2ddf82fca260da0b7dc39287155603
Sindre Dahl Løken <sindre.loken@nav.no>
2023-11-14 13:13:53 +0100

    lagt til alerts

---
+++ src/main/kotlin/no/nav/tiltak/tiltaknotifikasjon/brukernotifikasjoner/BrukernotifikasjonService.kt
@@ -0,0 +1,13 @@
+package no.nav.tiltak.tiltaknotifikasjon.brukernotifikasjoner
+
+import no.nav.tiltak.tiltaknotifikasjon.avtale.AvtaleHendelseMelding
+
+class BrukernotifikasjonService {
+
+    fun behandleAvtaleHendelseMelding(avtaleHendelse: AvtaleHendelseMelding) {
+//        when(avtaleHendelse.hendelseType) {
+//
+//        }
+    }
+
+}
