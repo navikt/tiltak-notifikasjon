2b469f4ca861a9dd3503f577df51127618d70931
Mattias Lundmark <lundmark.mattias@gmail.com>
2023-11-15 10:31:31 +0100

    Setter profil på kafka listener

--- src/main/kotlin/no/nav/tiltak/tiltaknotifikasjon/kafka/AvtaleHendelseConsumer.kt
+++ src/main/kotlin/no/nav/tiltak/tiltaknotifikasjon/kafka/AvtaleHendelseConsumer.kt
@@ -1,10 +1,12 @@
 package no.nav.tiltak.tiltaknotifikasjon.kafka
 
 import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Profile
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 
 @Component
+@Profile("dev-gcp", "dockercompose")
 class AvtaleHendelseConsumer {
     private val log = LoggerFactory.getLogger(javaClass)
 
