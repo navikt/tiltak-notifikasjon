27b4195a0923bbf98e2ed11129c45c13c048dfb9
Sindre Dahl Løken <sindre.loken@nav.no>
2023-11-15 10:28:46 +0100

    noe profil greier for lokal kjøring og tester

--- src/main/kotlin/no/nav/tiltak/tiltaknotifikasjon/TiltakNotifikasjonApplication.kt
+++ src/main/kotlin/no/nav/tiltak/tiltaknotifikasjon/TiltakNotifikasjonApplication.kt
@@ -2,10 +2,17 @@ package no.nav.tiltak.tiltaknotifikasjon
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import kotlin.system.exitProcess
 
 @SpringBootApplication
 class TiltakNotifikasjonApplication
 
 fun main(args: Array<String>) {
-	runApplication<TiltakNotifikasjonApplication>(*args)
+	runApplication<TiltakNotifikasjonApplication>(*args) {
+		if (System.getenv("MILJO") == null) {
+			println("Kan ikke startes uten miljøvariabel MILJO. Lokalt kan LokalTiltakRefusjonApplication kjøres.")
+			exitProcess(1)
+		}
+		setAdditionalProfiles(System.getenv("MILJO"))
+	}
 }
--- src/test/kotlin/no/nav/tiltak/tiltaknotifikasjon/DockerComposeTiltakNotifikasjonApplication.kt
+++ src/test/kotlin/no/nav/tiltak/tiltaknotifikasjon/DockerComposeTiltakNotifikasjonApplication.kt
@@ -2,9 +2,11 @@ package no.nav.tiltak.tiltaknotifikasjon
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Profile
 
 
 @SpringBootApplication
+@Profile("dockercompose")
 class DockerComposeTiltakNotifikasjonApplication
 
 fun main(args: Array<String>) {
