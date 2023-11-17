ff4c08834e7103e957e13cb9c07b6d94f9625c45
Sindre Dahl Løken <sindre.loken@nav.no>
2023-11-17 13:51:19 +0100

    min-side prefix på topic

--- src/main/kotlin/no/nav/tiltak/tiltaknotifikasjon/kafka/Topics.kt
+++ src/main/kotlin/no/nav/tiltak/tiltaknotifikasjon/kafka/Topics.kt
@@ -3,5 +3,5 @@ package no.nav.tiltak.tiltaknotifikasjon.kafka
 object Topics {
     const val AVTALE_HENDELSE = "arbeidsgiver.tiltak-avtale-hendelse"
     const val AVTALE_HENDELSE_COMPACT = "arbeidsgiver.tiltak-avtale-hendelse-compact"
-    const val BRUKERNOTIFIKASJON_BRUKERVARSEL = "aapen-brukervarsel-v1"
+    const val BRUKERNOTIFIKASJON_BRUKERVARSEL = "min-side.aapen-brukervarsel-v1"
 }
