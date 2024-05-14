5c0ff39b2cc9a1be3066ce443c1aa6387f20244a
Sindre Dahl Løken <sindre.loken@nav.no>
2024-05-14 12:32:45 +0200

    kun azure token henting i dev

--- src/main/kotlin/no/nav/tiltak/tiltaknotifikasjon/sikkerhet/AzureSecurityConfig.kt
+++ src/main/kotlin/no/nav/tiltak/tiltaknotifikasjon/sikkerhet/AzureSecurityConfig.kt
@@ -5,10 +5,12 @@ import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.web.reactive.function.client.WebClient
 
 @EnableOAuth2Client(cacheEnabled = true)
 @Configuration
+@Profile("dev-gcp")
 class AzureSecurityConfig {
 
     @Bean
