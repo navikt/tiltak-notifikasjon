358a83d6d030c1a4f4bf2f162720bcb5cedb9d06
Sindre Dahl Løken <sindre.loken@nav.no>
2024-05-14 15:46:26 +0200

    yyy

--- src/main/kotlin/no/nav/tiltak/tiltaknotifikasjon/sikkerhet/AzureSecurityConfig.kt
+++ src/main/kotlin/no/nav/tiltak/tiltaknotifikasjon/sikkerhet/AzureSecurityConfig.kt
@@ -18,7 +18,7 @@ class AzureSecurityConfig {
         clientConfigurationProperties: ClientConfigurationProperties,
         oAuth2AccessTokenService: OAuth2AccessTokenService
     ): WebClient.Builder {
-        val clientProperties = clientConfigurationProperties.registration.getValue("notifikasjoner")  // get("notifikasjoner") // .registration["notifikasjoner"]
+        val clientProperties = clientConfigurationProperties.registration["notifikasjoner"]
             ?: return WebClient.builder()// throw RuntimeException("could not find oauth2 client config for aad")
         return WebClient.builder().filter { request, next ->
             try {
