43c14a24227ed3b17239a98c137cf56d04a912ec
Sindre Dahl Løken <sindre.loken@nav.no>
2024-05-23 10:52:40 +0200

    oppdatert logging

--- src/main/kotlin/no/nav/tiltak/tiltaknotifikasjon/sikkerhet/AzureSecurityConfig.kt
+++ src/main/kotlin/no/nav/tiltak/tiltaknotifikasjon/sikkerhet/AzureSecurityConfig.kt
@@ -4,6 +4,7 @@ import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
@@ -14,6 +15,7 @@ import org.springframework.web.reactive.function.client.WebClient
 @Configuration
 @Profile("dev-gcp")
 class AzureSecurityConfig {
+    private val log = LoggerFactory.getLogger(javaClass)
 
     @Bean
     fun azureWebClientBuilder(
@@ -25,6 +27,7 @@ class AzureSecurityConfig {
         return WebClient.builder().filter { request, next ->
             try {
                 val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
+                log.info("Got response from oAuth2AccessTokenService.getAccessToken: $response")
                 request.headers().setBearerAuth(response.accessToken!!)
                 next.exchange(request)
             } catch (e: Exception) {
