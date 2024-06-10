7f268f577b854bf415fb0ca09d692dfa750bacb0
Sindre Dahl Løken <sindre.loken@nav.no>
2024-06-10 13:58:55 +0200

    hektet på arbeidsgivernotifikasjoner på kafkalytter og lagt på feature toggle

--- src/main/kotlin/no/nav/tiltak/tiltaknotifikasjon/kafka/AvtaleHendelseConsumer.kt
+++ src/main/kotlin/no/nav/tiltak/tiltaknotifikasjon/kafka/AvtaleHendelseConsumer.kt
@@ -2,6 +2,10 @@ package no.nav.tiltak.tiltaknotifikasjon.kafka
 
 import com.fasterxml.jackson.module.kotlin.readValue
 import io.getunleash.Unleash
+import no.nav.tiltak.tiltaknotifikasjon.arbeidsgivernotifikasjon.ArbeidsgiverNotifikasjonService
+import no.nav.tiltak.tiltaknotifikasjon.arbeidsgivernotifikasjon.Arbeidsgivernotifikasjon
+import no.nav.tiltak.tiltaknotifikasjon.arbeidsgivernotifikasjon.ArbeidsgivernotifikasjonRepository
+import no.nav.tiltak.tiltaknotifikasjon.arbeidsgivernotifikasjon.ArbeidsgivernotifikasjonStatus
 import no.nav.tiltak.tiltaknotifikasjon.avtale.AvtaleHendelseMelding
 import no.nav.tiltak.tiltaknotifikasjon.brukernotifikasjoner.Brukernotifikasjon
 import no.nav.tiltak.tiltaknotifikasjon.brukernotifikasjoner.BrukernotifikasjonRepository
@@ -19,7 +23,9 @@ import java.time.Instant
 @Profile("prod-gcp", "dev-gcp", "dockercompose")
 class AvtaleHendelseConsumer(
     val brukernotifikasjonService: BrukernotifikasjonService,
+    val arbeidsgiverNotifikasjonService: ArbeidsgiverNotifikasjonService,
     val brukernotifikasjonRepository: BrukernotifikasjonRepository,
+    val arbeidsgivernotifikasjonRepository: ArbeidsgivernotifikasjonRepository,
     val unleash: Unleash
 ) {
     private val mapper = jacksonMapper()
@@ -27,6 +33,12 @@ class AvtaleHendelseConsumer(
 
     @KafkaListener(topics = [Topics.AVTALE_HENDELSE_COMPACT])
     fun nyAvtaleHendelse(avtaleHendelse: String) {
+        behandleBrukernoitfikasjon(avtaleHendelse)
+        behandleArbeidsgivernotifikasjon(avtaleHendelse)
+    }
+
+    fun behandleBrukernoitfikasjon(avtaleHendelse: String) {
+        log.info("Behandle brukernotifikasjon")
         val erSkruddPå = unleash.isEnabled("sms-min-side-deltaker")
         if (!erSkruddPå) {
             log.info("Feature toggle sms-min-side-deltaker er skrudd av. Prosesserer ikke melding")
@@ -50,7 +62,31 @@ class AvtaleHendelseConsumer(
             brukernotifikasjonRepository.save(brukernotifikasjon)
             log.error("Error parsing AvtaleHendelseMelding: ${brukernotifikasjon.id}", e)
         }
+    }
+    fun behandleArbeidsgivernotifikasjon(avtaleHendelse: String) {
+        log.info("Behandle arbeidsgivernotifikasjon")
+        val erSkruddPå = unleash.isEnabled("arbeidsgivernotifikasjon-med-sak-og-sms")
+        if (!erSkruddPå) {
+            log.info("Feature toggle arbeidsgivernotifikasjon-med-sak-og-sms er skrudd av. Prosesserer ikke melding")
+            return
+        } else {
+            log.info("Feature toggle arbeidsgivernotifikasjon-med-sak-og-sms er skrudd på. Prosesserer melding")
+            try {
+                val melding: AvtaleHendelseMelding = mapper.readValue(avtaleHendelse)
+                arbeidsgiverNotifikasjonService.behandleAvtaleHendelseMelding(melding)
+            } catch (e: Exception) {
+                val arbeidsgivernotifikasjon = Arbeidsgivernotifikasjon(
+                    id = ulid(),
+                    avtaleMeldingJson = avtaleHendelse,
+                    status = ArbeidsgivernotifikasjonStatus.FEILET_VED_BEHANDLING,
+                    opprettet = Instant.now(),
+                    feilmelding = e.message
+                )
+                arbeidsgivernotifikasjonRepository.save(arbeidsgivernotifikasjon)
+                log.error("Error parsing AvtaleHendelseMelding for arbeidsgivernotifikasjon: ${arbeidsgivernotifikasjon.id}", e)
+            }
 
+        }
     }
 
 }
