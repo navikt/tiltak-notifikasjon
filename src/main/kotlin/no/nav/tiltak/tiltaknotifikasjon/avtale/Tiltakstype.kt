65771b3baf7dbe3ccf82172baf2a557b6668adb3
Sindre Dahl Løken <sindre.loken@nav.no>
2024-09-26 14:54:58 +0200

    tomme linjer

--- src/main/kotlin/no/nav/tiltak/tiltaknotifikasjon/avtale/Tiltakstype.kt
+++ src/main/kotlin/no/nav/tiltak/tiltaknotifikasjon/avtale/Tiltakstype.kt
@@ -11,8 +11,6 @@ enum class Tiltakstype(val beskrivelse: String, val arbeidsgiverNotifikasjonMerk
     SOMMERJOBB(beskrivelse = "Sommerjobb", arbeidsgiverNotifikasjonMerkelapp = "Sommerjobb");
 }
 
-
-
 fun Tiltakstype.serviceCode(altinnProperties: AltinnProperties): String {
     return when (this) {
         Tiltakstype.ARBEIDSTRENING -> altinnProperties.arbtreningServiceCode
