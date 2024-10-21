99c9b5adf24ba5c8b073d3f1a2cdd7683714d328
Sindre Dahl Løken <sindre.loken@nav.no>
2024-10-21 10:45:01 +0200

    brukte feil namespace på topic-navn...

--- src/main/kotlin/no/nav/tiltak/tiltaknotifikasjon/kafka/Topics.kt
+++ src/main/kotlin/no/nav/tiltak/tiltaknotifikasjon/kafka/Topics.kt
@@ -5,5 +5,5 @@ object Topics {
     const val AVTALE_HENDELSE_COMPACT = "arbeidsgiver.tiltak-avtale-hendelse-compact"
     const val BRUKERNOTIFIKASJON_BRUKERVARSEL = "min-side.aapen-brukervarsel-v1"
     const val BRUKERNOTIFIKASJON_HENDELSE = "min-side.aapen-varsel-hendelse-v1"
-    const val NOTIFIKASJON_KVITTERING = "arbeidsgiver.tiltak-notifikasjon-kvittering"
+    const val NOTIFIKASJON_KVITTERING = "team-tiltak.tiltak-notifikasjon-kvittering"
 }
