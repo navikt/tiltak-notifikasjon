cd4e7a72e71f3042013a8365a0450f7653cd54de
Eirik Vigeland <eirik.vigeland@nav.no>
2026-03-13 12:28:05 +0100

    fix. legge til indekser på id'er vi henter mye av
    * Legge til indeks på avtale-id for brukernotifikasjon og arbeidsgivernotifikasjon
    * Legge til indeks på varsel_id for brukernotifikasjon
    * Legge til indeks på response_id for arbeidsgivernotifikasjon

---
+++ src/main/resources/db/migration/V8__indekser.sql
@@ -0,0 +1,5 @@
+-- Indekser på ofte brukte kolonner i spørringer
+CREATE INDEX idx_brukernotifikasjon_avtale_id ON brukernotifikasjon (avtale_id);
+CREATE INDEX idx_brukernotifikasjon_varsel_id ON brukernotifikasjon (varsel_id);
+CREATE INDEX idx_arbeidsgivernotifikasjon_avtale_id ON arbeidsgivernotifikasjon (avtale_id);
+CREATE INDEX idx_arbeidsgivernotifikasjon_response_id ON arbeidsgivernotifikasjon (response_id);
