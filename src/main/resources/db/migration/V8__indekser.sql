9a7b3ebedbba81ad3b4926dcf7ed15d1ac039113
Sindre Dahl Løken <sindre.loken@nav.no>
2026-03-16 10:27:00 +0100

    concurrently stottes ikke av jooq-h2 (#43)

--- src/main/resources/db/migration/V8__indekser.sql
+++ src/main/resources/db/migration/V8__indekser.sql
@@ -1,5 +1,5 @@
 -- Indekser på ofte brukte kolonner i spørringer
-CREATE INDEX CONCURRENTLY idx_brukernotifikasjon_avtale_id ON brukernotifikasjon (avtale_id);
-CREATE INDEX CONCURRENTLY idx_brukernotifikasjon_varsel_id ON brukernotifikasjon (varsel_id);
-CREATE INDEX CONCURRENTLY idx_arbeidsgivernotifikasjon_avtale_id ON arbeidsgivernotifikasjon (avtale_id);
-CREATE INDEX CONCURRENTLY idx_arbeidsgivernotifikasjon_response_id ON arbeidsgivernotifikasjon (response_id);
+CREATE INDEX IF NOT EXISTS idx_brukernotifikasjon_avtale_id ON brukernotifikasjon (avtale_id);
+CREATE INDEX IF NOT EXISTS idx_brukernotifikasjon_varsel_id ON brukernotifikasjon (varsel_id);
+CREATE INDEX IF NOT EXISTS idx_arbeidsgivernotifikasjon_avtale_id ON arbeidsgivernotifikasjon (avtale_id);
+CREATE INDEX IF NOT EXISTS idx_arbeidsgivernotifikasjon_response_id ON arbeidsgivernotifikasjon (response_id);
