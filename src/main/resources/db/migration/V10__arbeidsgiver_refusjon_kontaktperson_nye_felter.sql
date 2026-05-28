0524a8dab819c5e586e9646987201a2b0a27e015
Sindre Dahl Løken <sindre.loken@nav.no>
2026-05-28 10:30:05 +0200

    wip

--- src/main/resources/db/migration/V10__arbeidsgiver_refusjon_kontaktperson_nye_felter.sql
+++ src/main/resources/db/migration/V10__arbeidsgiver_refusjon_kontaktperson_nye_felter.sql
@@ -1,4 +1,5 @@
 alter table arbeidsgiver_refusjon_kontaktperson
     add column arbeidsgiver_tlf varchar,
-    add column tiltakstype      varchar,
-    alter column refusjon_kontaktperson_tlf drop not null;
+    add column tiltakstype      varchar;
+
+alter table arbeidsgiver_refusjon_kontaktperson alter column refusjon_kontaktperson_tlf drop not null;
