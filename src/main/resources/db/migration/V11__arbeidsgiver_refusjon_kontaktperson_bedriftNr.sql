bb26c637aeb3a43d73ffbe433e44c1389d9ae84e
Sindre Dahl Løken <sindre.loken@nav.no>
2026-05-29 13:51:31 +0200

    wops

--- src/main/resources/db/migration/V11__arbeidsgiver_refusjon_kontaktperson_bedriftNr.sql
+++ src/main/resources/db/migration/V11__arbeidsgiver_refusjon_kontaktperson_bedriftNr.sql
@@ -1,4 +1,4 @@
--- Tømmer tabellen siden dataene ikke er i bruk ennå, og backfill kjøres på nytt med nye felter slik vi kan ha not null på tiltakstype som nå blir lagt til.
+-- Tømmer tabellen siden dataene ikke er i bruk ennå, og backfill kjøres på nytt med nye felter slik vi kan ha not null på bedriftNr som nå blir lagt til.
 truncate table arbeidsgiver_refusjon_kontaktperson;
 
 alter table arbeidsgiver_refusjon_kontaktperson add column bedriftNr varchar not null;
